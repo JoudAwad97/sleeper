9a4a073ebb0449d8da9e9944537dd67b4c6a476f
Joud Awad <joud.awad@metalabdesign.com>
2023-12-25 21:49:52 +0300

    first commit

---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      // allow us to run a code with dependency injection to initialize the module
+      // Returned object should be a module configuration object
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
+      // list of dependencies that are required to execute the factory function
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
