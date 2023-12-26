952ecf1e375386642634a88a97c589b559ffeb75
Joud Awad <joud.awad@metalabdesign.com>
2023-12-26 22:08:08 +0300

    start installing the database module

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,16 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
 
 @Module({
   imports: [
-    MongooseModule.forRootAsync({
+    TypeOrmModule.forRootAsync({
       // allow us to run a code with dependency injection to initialize the module
       // Returned object should be a module configuration object
       useFactory: (configService: ConfigService) => ({
-        uri: configService.get('MONGODB_URI'),
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        type: 'mysql',
+        host: configService.getOrThrow('DB_HOST'),
+        port: configService.getOrThrow('DB_PORT'),
+        username: configService.getOrThrow('DB_USERNAME'),
+        password: configService.getOrThrow('MYSQL_ROOT_PASSWORD'),
+        database: configService.getOrThrow('DB_DATABASE'),
+        // do not use "synchronize" in production - otherwise you can lose production data
+        synchronize: true,
+        autoLoadEntities: true,
       }),
       // list of dependencies that are required to execute the factory function
       inject: [ConfigService],
@@ -18,7 +25,7 @@ import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
   ],
 })
 export class DatabaseModule {
-  static forFeature(models: ModelDefinition[]) {
-    return MongooseModule.forFeature(models);
+  static forFeature(models: EntityClassOrSchema[]) {
+    return TypeOrmModule.forFeature(models);
   }
 }
